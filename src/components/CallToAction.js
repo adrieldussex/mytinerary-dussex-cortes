04daa10243db39964e0da3593ed93a1413aefab8
adrieldussex <adrieldussex@gmail.com>
2022-08-19 18:17:03 -0300

    Call to action JS

---
+++ src/components/CallToAction.js
@@ -0,0 +1,13 @@
+import '../styles/CallToAction.css'
+
+export default function CallToAction() {
+  return (
+    // <a href="#" className='Hero-calltoaction'>Find your next destination</a>
+    <>    
+      <a className="CallToAction-button">
+        <img height="24" width="24" src="https://i.imgur.com/OZpDvUU.png" alt=""></img>
+        <span>Find your next destination</span>
+      </a>
+    </>
+  )
+}
