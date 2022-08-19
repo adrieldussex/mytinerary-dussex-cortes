957ccdd9ce7a23d908c4092023d0aa87954ab02b
adrieldussex <adrieldussex@gmail.com>
2022-08-19 17:05:32 -0300

    fixed page

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Hero from './pages/Hero';
-import Footer from './components/Footer';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hero />
-    <Footer />
+    <App />
   </React.StrictMode>
 );
 
