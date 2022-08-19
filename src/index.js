783c598c444e20c60995be6d43d4abb813c42118
lec89 <lucascorts@hotmail.com>
2022-08-19 19:17:51 -0300

    index.js modified

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Hero from './pages/Hero';
-import Footer from './components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hero />
-    <Footer />
+    <App />
   </React.StrictMode>
 );
 
