b419e815502e836bddff96d76595515b3be98d74
lec89 <lucascorts@hotmail.com>
2022-08-20 14:50:57 -0300

    Carousel.js added

---
+++ src/components/Carousel.js
@@ -0,0 +1,24 @@
+import '../styles/Carousel.css'
+
+export default function Carousel(props) {
+    const range = props.range
+    const start = 0
+    const end = start + range
+    const items = props.data
+
+    const itemView = (item) => (
+        <div className='item'>
+            <img src={item.url} />
+            <p>{item.title}</p>
+        </div>
+    )
+    
+  return (
+    <div className='Carousel'>
+        <h1>Popular MyTineraries</h1>
+        <div className='Carousel-slide'>
+            { items.slice(start, end).map(itemView) }
+        </div>
+    </div>
+  )
+}
