57b5c1901a3519dc33d7ee2f60633938727d81df
lec89 <lucascorts@hotmail.com>
2022-08-20 14:51:52 -0300

    Footer.js modified

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import '../styles/Footer.css';
+import Logo from './img/Logo-nav.png';
 
 export default function Footer() {
   return (
     <footer className='Footer-container'>
-        <img className='Footer-logo' src='' alt='logo'></img>
-        <h3 className='Footer-title'>MyTinerary | 2022</h3>
+        <img className='Footer-logo' src={Logo} alt='logo'></img>
+        <h3 className='Footer-title'>2022</h3>
         <div className='Footer-links'>
             <a className='Footer-links-item' href='#'>Home</a>
             <a className='Footer-links-item' href='#'>Cities</a>
