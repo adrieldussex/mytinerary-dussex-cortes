6b340740b6e604bd3b183aafabb7e938e187b9ac
lec89 <lucascorts@hotmail.com>
2022-08-19 22:28:57 -0300

    Carousel modified

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,18 +1,23 @@
+import '../styles/Carousel.css'
+
 export default function Carousel(props) {
-    const range = 4
-    const items = props.items
+    const range = props.range
+    const start = 0
+    const end = start + range
+    const items = props.data
 
     const itemView = (item) => (
         <div className='item'>
-            <img src={item.url}></img>
+            <img src={item.url} />
             <p>{item.title}</p>
         </div>
     )
     
   return (
     <div>
-        <div className='slide'>
-            { items.slice(0, range).map(itemView) }
+        <div className='Carousel-slide'>
+            <h1>Popular MYtineraries</h1>
+            { items.slice(start, end).map(itemView) }
         </div>
     </div>
   )
