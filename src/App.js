0e876580ebeb67a1eb37cd634230686b47216658
lec89 <lucascorts@hotmail.com>
2022-08-21 16:53:34 -0300

    App.js modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import Hero from './pages/Hero';
 import Footer from './components/Footer';
 import CitiesCarousel from './components/CitiesCarousel'
-
 
 function App() {
   return (
