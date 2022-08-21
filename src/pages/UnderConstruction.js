8bbc27701e483beeef7ca60e59e622fe70ffaea6
lec89 <lucascorts@hotmail.com>
2022-08-21 16:50:25 -0300

    UnderConstruction.js added

---
+++ src/pages/UnderConstruction.js
@@ -0,0 +1,13 @@
+import '../styles/UnderConstruction.css'
+import WebsiteLayout from '../layouts/WebsiteLayout'
+
+export default function UnderConstruction() {
+  return (
+    <WebsiteLayout>
+        <div className='UnderConstruction-container'>
+            <h1>UNDER CONSTRUCTION</h1>
+            <h2>Our web site is coming soon...</h2>
+        </div>
+    </WebsiteLayout>
+  )
+}
