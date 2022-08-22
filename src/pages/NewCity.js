b1c0fef82db2aea48deaf0bcb054687629bd5076
lec89 <lucascorts@hotmail.com>
2022-08-22 18:41:27 -0300

    NewCity.js added

---
+++ src/pages/NewCity.js
@@ -0,0 +1,12 @@
+import '../styles/NewCity.css'
+import WebsiteLayout from '../layouts/WebsiteLayout'
+
+export default function NewCity() {
+  return (
+    < WebsiteLayout>
+        <div className='NewCity-container'>
+            <h1>New City</h1>
+        </div>
+    </WebsiteLayout>
+  )
+}
