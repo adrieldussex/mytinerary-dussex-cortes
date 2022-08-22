8d849dcc76b15ec0f86cacf20c5de947b6234d4d
lec89 <lucascorts@hotmail.com>
2022-08-22 18:36:24 -0300

    NewCities.js added

---
+++ src/pages/NewCities.js
@@ -0,0 +1,12 @@
+import '../styles/NewCities.css'
+import WebsiteLayout from '../layouts/WebsiteLayout'
+
+export default function NewCities() {
+  return (
+    < WebsiteLayout>
+        <div className='NewCities-container'>
+            <h1>New Cities</h1>
+        </div>
+    </WebsiteLayout>
+  )
+}
