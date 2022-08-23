e0bf0fbad11615216d90455759f2edcf202af1eb
adrieldussex <adrieldussex@gmail.com>
2022-08-23 19:08:33 -0300

    App new link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Hero from './pages/Hero';
 import Cities from './pages/Cities';
+import NewCity from './pages/NewCity';
 import WebsiteLayout from './layouts/WebsiteLayout';
 import UnderConstruction from './pages/UnderConstruction'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Hero />}/>
           <Route path='/cities' element={<Cities />}/>
+          <Route path='/newcity' element={<NewCity />}/>
           <Route path='/*' element={<UnderConstruction />}/>
         </Routes>
       </WebsiteLayout>
