b87028823cbc116d09dbaa51b2c8da1213418bd6
adrieldussex <adrieldussex@gmail.com>
2022-08-23 15:56:58 -0300

    Routes fixed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import Hero from './pages/Hero';
 import Cities from './pages/Cities';
-import { BrowserRouter } from 'react-router-dom';
+import WebsiteLayout from './layouts/WebsiteLayout';
+import UnderConstruction from './pages/UnderConstruction'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Hero />}/>
-        <Route path='/cities' element={<Cities />}/>
-      </Routes>
+      <WebsiteLayout>
+        <Routes>
+          <Route path='/' element={<Hero />}/>
+          <Route path='/cities' element={<Cities />}/>
+          <Route path='/*' element={<UnderConstruction />}/>
+        </Routes>
+      </WebsiteLayout>
     </BrowserRouter>
   );
 }
--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,13 +1,14 @@
 import '../styles/CallToAction.css'
+import {Link as LinkRouter} from 'react-router-dom'
 
-export default function CallToAction() {
+export default function CallToAction(props) {
+    let linkTo = props.linkTo
   return (
-    // <a href="#" className='Hero-calltoaction'>Find your next destination</a>
     <>    
-      <a className="CallToAction-button">
+      <LinkRouter className="CallToAction-button" to={linkTo}>
         <img height="24" width="24" src="https://i.imgur.com/OZpDvUU.png" alt=""></img>
         <span>Find your next destination</span>
-      </a>
+      </LinkRouter>
     </>
   )
 }
