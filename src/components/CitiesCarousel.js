615479aa2fea6c177ef233764823a5e57360a138
lec89 <lucascorts@hotmail.com>
2022-08-24 12:32:47 -0300

    city modified

--- src/components/CitiesCarousel.js
+++ src/components/CitiesCarousel.js
@@ -8,7 +8,7 @@ export default function CitiesCarousel() {
         {url: "https://www.planetware.com/wpimages/2020/03/world-most-visited-cities-london-england.jpg", title: "London, England"},
         {url: "https://www.planetware.com/wpimages/2020/03/world-most-visited-cities-bangkok-thailand.jpg", title: "Bangkok, Thailand"},
         {url: "https://www.planetware.com/wpimages/2020/03/world-most-visited-cities-hong-kong.jpg", title: "Hong Kong, China"},
-        {url: "https://www.planetware.com/wpimages/2020/03/world-most-visited-cities-dubai-united-arab-emirates.jpg", title: "Dubai, United Arab Emirates"},
+        {url: "https://www.planetware.com/wpimages/2020/03/denmark-in-pictures-beautiful-places-to-photograph-nyhavn-copenhagen.jpg", title: "Copenhagen, Denmark"},
         {url: "https://www.planetware.com/wpimages/2020/03/world-most-visited-cities-singapore.jpg", title: "Singapore"},
         {url: "https://www.planetware.com/wpimages/2020/03/world-most-visited-cities-rome-italy.jpg", title: "Rome, Italy"},
         {url: "https://www.planetware.com/wpimages/2020/03/world-most-visited-cities-macau-china.jpg", title: "Macau, China"},
