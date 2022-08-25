2a3889c0ff83d2db0c387c2a9b583f9412c853d9
lec89 <lucascorts@hotmail.com>
2022-08-24 22:20:00 -0300

    header fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import '../styles/Header.css';
 import MobileNav from './MobileNav';
 import Nav from './Nav';
-import {Link as LinkRouter} from 'react-router-dom'
+import { Link as LinkRouter } from 'react-router-dom'
 
 export default function Header() {
     return (
@@ -9,11 +9,11 @@ export default function Header() {
             <LinkRouter to='/'>
                 <img className='Header-logo' src='http://localhost:3000/img/Logo-nav.png' alt="logo-header"></img>
             </LinkRouter>
-            <div className='Header-right'>                
+            <div className='Header-right'>
                 <MobileNav />
                 <Nav />
-            </div>
                 <img className='Header-avatar' src="https://i.imgur.com/CNe5NKD.png" alt="user-avatar"></img>
+            </div>
         </div>
     )
 }
