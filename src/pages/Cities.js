8c38b846d623108e6d1806bb92fa9412af935517
adrieldussex <adrieldussex@gmail.com>
2022-08-26 16:24:10 -0300

    Cities Page

--- src/pages/Cities.js
+++ src/pages/Cities.js
@@ -1,9 +1,14 @@
+import CityCard from '../components/CityCard'
 import '../styles/Cities.css'
+
 
 export default function Cities() {
   return (
       <div className='Cities-container'>
           <h1>Cities</h1>
+          <div className='Cities-card-container'>
+            < CityCard />
+          </div>
       </div>
   )
 }
