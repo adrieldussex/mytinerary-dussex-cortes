d02022bb72a8ebf08972215f7431a646869f8d6c
adrieldussex <adrieldussex@gmail.com>
2022-08-31 18:14:53 -0300

    Details JS

---
+++ src/components/CityDetails.js
@@ -0,0 +1,17 @@
+import '../styles/CityDetails.css'
+
+export default function CityDetails() {
+return (
+<div className="CityDetails-container">
+    <div className='CityDetails-img'>
+        <img src="https://guias-viajar.com/wp-content/uploads/2021/04/Avenida-9-de-julio-Buenos-Aires-en-Argentina.jpg" alt=""></img>
+    </div>
+    <div className='CityDetails-info'>
+        <h3>Paris</h3>
+        <h3>France</h3>
+        <p>Population</p>
+        <p>Fundation</p>
+    </div>
+</div>
+)
+}
