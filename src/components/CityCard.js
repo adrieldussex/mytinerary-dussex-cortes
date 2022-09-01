eaebffa853410dbe7b674cee7e09bce0175660c8
lec89 <lucascorts@hotmail.com>
2022-09-01 00:56:09 -0300

    cityCard modified

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -2,16 +2,13 @@ import '../styles/CityCard.css'
 import {Link as LinkRouter} from 'react-router-dom'
 
 export default function CityCard(props) {
-    const country = props.country
-    const city = props.city
-    const photo = props.photo
-
+    const item = props
     return (
-        <div className='CityCard-container' key={city}>
-            <img src={photo} alt={city}/>
+        <div className='CityCard-container' key={item.city}>
+            <img src={item.photo} alt={item.city}/>
             <div className='CityCard-info'>
-                <p>{city}</p>
-                <p>{country}</p>
+                <p>{item.city}</p>
+                <p>{item.country}</p>
             </div>
             <div className='CityCard-details'>
                 <LinkRouter to='/details'>See more</LinkRouter>
