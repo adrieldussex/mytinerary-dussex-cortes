c6e9ecabf1336a70b716883a8e2ceff462392f2e
adrieldussex <adrieldussex@gmail.com>
2022-09-07 16:32:55 -0300

    Modified Itinerary. Add activities

--- src/components/Itinerary.js
+++ src/components/Itinerary.js
@@ -1,7 +1,9 @@
 import '../styles/Itinerary.css'
+import Activities from './Activities'
 
 export default function Itinerary() {
   return (       
+    <div className='Itinerary-container'>        
         <div className='Itinerary-card'>
             <div className='Itinerary-photo' style={{backgroundImage:"url('http://mytinerary-arguello.herokuapp.com/assets/imgs/pp-domi.png')", backgroundSize:"cover"}}></div>
             <div className='Itinerary-description'>                
@@ -24,7 +26,9 @@ export default function Itinerary() {
             </div>
             <div>
                 <img className='Like' src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAW9JREFUSEvVlUFWwjAQhr+h7OUIeALxBOIN5Alr5QR6FD0BurY+uIF4g3oCOALugfElaWoorQ0L9Jld8qf/P/NnZiocecmR+fkjgVSvgBugl2e4BB4ZyszuHX4HdHM8A54LPLBlN4OpdtgwETAEe0thbg4F+jX4jIQxA1l5fFcg1UzgTJVPWtzTcoRs6bPlQYQTsy3wDSZySOh5XJWMkZzvC7zqrSgT+3GbbhiFveyyc4IJ/Up8zdIEocKYa3nKs821XnQuwkUIHlxh30G+MxJrY2GRpKo2/YRTBmIe9fA11a5sWFieoVju3xPg2Bbh/YMVzqai1KK8ckWwEOhUP7JrIF+mGW0uo0UM+Zo3EXoKHwzFN2hpVOSlmPdCnEiZvFTC+7PoEJEG8t0+CI2OEYkgrxcIOrfSrkjynwXqRMx5+KBVYyNwo/l/ULbLdKevlgby5gx8JIGIHQOmFCPI4wVipmlNNzZbFNXG9Zf+v8AXmgjeGbN7Px8AAAAASUVORK5CYII="/>
-            </div>               
+            </div>   
         </div>
+        <Activities/>                       
+    </div>
   )
 }
