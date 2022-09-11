0314ded4e87e9702864909368908497d4fabbdf7
lec89 <lucascorts@hotmail.com>
2022-09-10 22:51:35 -0300

    label modified

--- src/pages/NewCity.js
+++ src/pages/NewCity.js
@@ -3,18 +3,23 @@ import Input from '../components/Input'
 
 export default function NewCity() {
   const form = [
-    {label: 'Country', for: 'country', type: 'text'},
-    {label: 'City', for: 'city', type: 'text'},
-    {label: 'Population', for: 'population', type: 'number'},
-    {label: 'Photo', for: 'photo', type: 'text'},
-    {label: 'Founded', for: 'founded', type: 'date'}
+    { label: 'City', htmlFor: 'city', type: 'text', placeholder: 'Enter a city' },
+    { label: 'Country', htmlFor: 'country', type: 'text', placeholder: 'Enter a country' },
+    { label: 'Population', htmlFor: 'population', type: 'number', placeholder: 'Enter a population' },
+    { label: 'Photo', htmlFor: 'photo', type: 'text', placeholder: 'Enter a photo' },
+    { label: 'Founded', htmlFor: 'founded', type: 'date', placeholder: 'Enter a founded' }
   ]
+  const submit = (e) => {
+    e.preventDefault()
+
+  }
   return (
-        <div className='NewCity-container'>
-            <h1>New City</h1>
-            <div className='NewCity-form'> 
-              {form.map(input => <Input label={input.label} htmlFor={input.for} type={input.type}/>)}
-            </div>
-        </div>
+    <div className='NewCity-container'>
+      <h1>New City</h1>
+      <form className='NewCity-form'>
+        {form.map(input => <Input label={input.label} htmlFor={input.htmlFor} type={input.type} key={input.label} />)}
+        <button className='Submit-button' type='submit'>Add City</button>
+      </form>
+    </div>
   )
 }
