1c1a6cabf3dbe3e88675494f85b31baa3422ee4b
Richard Reyes <30903763+ReyesRRV@users.noreply.github.com>
2022-09-12 19:14:33 -0300

    Create new city

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,11 +4,12 @@ export default function Input(props) {
   let inputLabel = props.label
   let inputFor = props.for
   let inputType = props.type
+  let inputValue = props.value
 
   return (
     <div className='Input-wrapper' key={inputLabel}>
       <label htmlFor={inputFor}>{inputLabel}</label>
-      <input className="Input-input" type={inputType} id={inputFor} name={inputFor}></input>
+      <input className="Input-input" type={inputType} id={inputFor} name={inputFor} ref={inputValue}></input>
     </div>
   )
 }
--- src/pages/NewCity.js
+++ src/pages/NewCity.js
@@ -1,25 +1,98 @@
-import '../styles/NewCity.css'
-import Input from '../components/Input'
+import React from "react";
+import "../styles/NewCity.css";
+import Input from "../components/Input";
+import { useCreateCityMutation } from "../features/citiesAPI";
+import { useRef } from "react";
+
+
 
 export default function NewCity() {
-  const form = [
-    { label: 'City', htmlFor: 'city', type: 'text', placeholder: 'Enter a city' },
-    { label: 'Country', htmlFor: 'country', type: 'text', placeholder: 'Enter a country' },
-    { label: 'Population', htmlFor: 'population', type: 'number', placeholder: 'Enter a population' },
-    { label: 'Photo', htmlFor: 'photo', type: 'text', placeholder: 'Enter a photo' },
-    { label: 'Founded', htmlFor: 'founded', type: 'date', placeholder: 'Enter a founded' }
-  ]
-  const submit = (e) => {
-    e.preventDefault()
 
+  const [createCity] = useCreateCityMutation()
+
+
+  const cityPhotoRef = useRef()
+  const cityNameRef = useRef()
+  const countryNameRef = useRef()
+  const populationRef = useRef()
+  const foundationRef = useRef()
+
+  const form = [
+    {
+      label: "City",
+      htmlFor: "city",
+      type: "text",
+      placeholder: "Enter a city",
+      value: cityNameRef,
+    },
+    {
+      label: "Country",
+      htmlFor: "country",
+      type: "text",
+      placeholder: "Enter a country",
+      value: countryNameRef,
+    },
+    {
+      label: "Population",
+      htmlFor: "population",
+      type: "number",
+      placeholder: "Enter a population",
+      value: populationRef,
+    },
+    {
+      label: "Photo",
+      htmlFor: "photo",
+      type: "text",
+      placeholder: "Enter a photo",
+      value: cityPhotoRef,
+    },
+    {
+      label: "Founded",
+      htmlFor: "founded",
+      type: "date",
+      placeholder: "Enter a founded",
+      value: foundationRef,
+    },
+  ];
+
+
+  function newCity(e)  {
+    e.preventDefault();
+
+    const dataCity = {
+      city: cityNameRef.current.value,
+      photo: cityPhotoRef.current.value,
+      country: countryNameRef.current.value,
+      population: populationRef.current.value,
+      founded: foundationRef.current.value
   }
+  console.log(dataCity)
+  createCity(dataCity)
+
+  ///Get better option for a new city///
+alert("Created successfully")
+
+///Add a reset here =) ♥
+}
+
   return (
-    <div className='NewCity-container'>
+    <div className="NewCity-container">
       <h1>New City</h1>
-      <form className='NewCity-form'>
-        {form.map(input => <Input label={input.label} htmlFor={input.htmlFor} type={input.type} key={input.label} />)}
-        <button className='Submit-button' type='submit'>Add City</button>
+      <form className="NewCity-form" onSubmit={newCity}>
+        {form.map((input) => (
+          <Input
+            label={input.label}
+            htmlFor={input.htmlFor}
+            type={input.type}
+            key={input.label}
+            value={input.value}
+          />
+        ))}
+        <button className="Submit-button" type="submit">
+          Add City
+        </button>
       </form>
     </div>
-  )
-}
+  );
+
+        }
