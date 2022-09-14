177089e3636cbb96d57827c218000bc848d90129
Richard Reyes <30903763+ReyesRRV@users.noreply.github.com>
2022-09-14 00:20:39 -0300

    MyTinerary page working =)

--- src/pages/MyTinerares.js
+++ src/pages/MyTinerares.js
@@ -1,7 +1,50 @@
-import React from 'react'
+import React from "react";
+import { useGetItineraryByCUMutation } from "../features/itineraryAPI";
+import { useEffect, useState } from "react";
+import Itinerary from "../components/Itinerary";
 
 export default function MyTinerares() {
+  const [id, setId] = useState("63181031f35384d38640571f");
+  
+  const [getAllItinerary, { data: items}] = useGetItineraryByCUMutation();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+   
+  useEffect(() => {
+    getAllItinerary({ city: "", user: id });
+  }, []  );
+  
+  console.log(items)
+
+
   return (
-    <div>MyTinerares</div>
+    <>
+      {/* <Itinerary data={items}/> */}
+    {items?.map(Itinerary)}
+    </>
   )
 }
--- src/store.js
+++ src/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import citiesAPI from './features/citiesAPI'
+import citiesAPI from "./features/citiesAPI";
+import itineraryAPI from "./features/itineraryAPI";
 
 export default configureStore({
-    reducer: {
-        [citiesAPI.reducerPath] : citiesAPI.reducer
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(citiesAPI.middleware)
-})
+  reducer: {
+    [citiesAPI.reducerPath]: citiesAPI.reducer,
+    [itineraryAPI.reducerPath]: itineraryAPI.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(citiesAPI.middleware),
+});
