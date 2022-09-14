1ff9a6410ab8f9053181dd5aa25dd11fe09febe3
lec89 <lucascorts@hotmail.com>
2022-09-14 02:51:36 -0300

    sign in google added

---
+++ src/components/SignInGoogle.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef } from 'react'
+import * as jose from 'jose'
+
+export default function SignInGoogle() {
+    const buttonDiv = useRef(null)
+    
+    async function handleCredentialResponse(response) {
+        let userObject = jose.decodeJwt(response.credential)
+    
+        let data  = {
+            mail: userObject.email,
+            pass: userObject.sub,
+            role: 'user',
+            from: 'google',
+        } // newUser(data)
+    }
+    
+    useEffect(() => {
+
+        /* global google */
+        google.accounts.id.initialize({
+            client_id: "919249866967-qffo42lhcei7m2472ut3sntdmgm23umi.apps.googleusercontent.com",
+            callback: handleCredentialResponse,
+            context: "use"
+          });
+
+          google.accounts.id.renderButton(
+            buttonDiv.current,
+            { theme: "outline", size: "large", text: "continue_with", shape: "pill" }
+          );
+    }, [])
+
+  return (
+    <div ref={buttonDiv}></div>
+  )
+}
