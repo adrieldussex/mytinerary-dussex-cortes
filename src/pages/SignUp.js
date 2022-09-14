41bfad0f6160646ec7b836b37e6bbc709a5e3a7f
lec89 <lucascorts@hotmail.com>
2022-09-14 02:50:03 -0300

    sign up added

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,34 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SignUpGoogle from '../components/SignUpGoogle'
 import '../styles/SignUp.css'
 import { Link as LinkRouter } from 'react-router-dom'
-import Input from '../components/Input'
-
 
 export default function SignUp() {
+  const [name, setName] = useState('')
+  const [lname, setLname] = useState('')
+  const [photo, setPhoto] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div className='SignUp-container'>
-        <div className='SignUp-register'>
-          <LinkRouter to='/'>
-            <img className='Header-logo' src='/img/Logo-nav.png' alt="logo-header"></img>
-          </LinkRouter>
-          <h3>
-            Sign up to see and create itineraries with your friends
-          </h3>
-          <SignUpGoogle/>
-          <div className='Register-divisor'>
-            <hr></hr>
-            <h3>OR</h3>
-            <hr></hr>
-          </div>
-          <Input />
-          <p>By signing up, you agree to our Terms , Privacy Policy and Cookies Policy .</p>
-          <button className='Submit-button' type='submit'>Sign Up</button>
+      <form className='SignUp-register' onSubmit={handleSubmit}>
+        <h3>
+          Sign up to see and create itineraries with your friends
+        </h3>
+        <SignUpGoogle />
+        <div className='Register-divisor'>
+          <hr></hr>
+          <h3>OR</h3>
+          <hr></hr>
         </div>
-        <div className='SignUp-logIn'>
-          <p>Have an account?</p>
-          <a href="#">Log In</a>
-        </div>
+        <input className='Input' name='Name' placeholder='Name' type='text' required></input>
+        <input className='Input' name='LastName' placeholder='Last Name' type='text' required></input>
+        <input className='Input' name='photo' placeholder='URL Photo' type='' required></input>
+        <input className='Input' name='email' placeholder='Email' type='email' required></input>
+        <input className='Input' name='pass' placeholder='Password' type='password' required></input>
+        <button className='Submit-button' type='submit'>Create Account</button>
+        
+      </form>
+      <div className='SignUp-logIn'>
+        <p>Do you have an account?</p>
+        <LinkRouter to='/auth/signin'>Log In</LinkRouter>
+      </div>
     </div>
   )
 }
