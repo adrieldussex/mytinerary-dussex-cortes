10027555d7f0c331b8763c9e988e818b968c5196
adrieldussex <adrieldussex@gmail.com>
2022-09-21 16:40:17 -0300

    Add Comment in Store

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import citiesAPI from "./features/citiesAPI";
 import itineraryAPI from "./features/itineraryAPI";
 import userAPI from "./features/usersAPI";
+import { commentsAPI } from "./features/commentsAPI";
 import { activitiesAPI } from "./features/activitiesAPI";
 
 export default configureStore({
@@ -10,11 +11,13 @@ export default configureStore({
     [itineraryAPI.reducerPath]: itineraryAPI.reducer,
     [userAPI.reducerPath]: userAPI.reducer,
     [activitiesAPI.reducerPath]: activitiesAPI.reducer,
+    [commentsAPI.reducerPath]: commentsAPI.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
     .concat(citiesAPI.middleware)
     .concat(itineraryAPI.middleware)
     .concat(userAPI.middleware)
-    .concat(activitiesAPI.middleware),
+    .concat(activitiesAPI.middleware)
+    .concat(commentsAPI.middleware),
 });
