1080368b78185cf945d84256c70848da6de8f0af
lec89 <lucascorts@hotmail.com>
2022-09-21 15:15:33 +0200

    line removed

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,6 @@ import { useRef, useState, useEffect } from "react";
 export default function Header() {
   const [open, setOpen] = useState(false);
   const menuIcon = useRef(null);
- 
 
   const handleCloseMenu = (event) => {
     const isClickInside = menuIcon.current.contains(event.target);
