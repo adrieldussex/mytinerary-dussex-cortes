3e23832c511d831d92c48ec46f4f44219a3ab33f
adrieldussex <adrieldussex@gmail.com>
2022-09-21 11:43:02 -0300

    Modified component itinerary

--- src/components/Itinerary.js
+++ src/components/Itinerary.js
@@ -6,30 +6,6 @@ import Comments from "./Comments";
 export default function Itinerary(item) {
   return (
     <div className="Itinerary-container" key={item._id}>
-      {/* <div className="Itinerary-card">
-        <div className="Itinerary-title">
-          <h3>{item.name}</h3>
-        </div>
-        <div className="Itinerary-user">
-            <img className='Itinerary-photo' src={item.user.photo} alt={item.user.name}></img>
-            <p>{item.user.name}</p>
-        </div>
-        <div className="Itinerary-description">
-          <div className="Itinerary-info">
-            <div className="Info-price-duration">
-              <p>Price: {item.price}</p>
-              <p>Duration: {item.duration}</p>
-            </div>
-            <div className="Info-tags">
-              <p> {item.tags} </p>
-            </div>
-          </div>
-        </div>
-        <div>
-          {/* <img className='Like' src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAW9JREFUSEvVlUFWwjAQhr+h7OUIeALxBOIN5Alr5QR6FD0BurY+uIF4g3oCOALugfElaWoorQ0L9Jld8qf/P/NnZiocecmR+fkjgVSvgBugl2e4BB4ZyszuHX4HdHM8A54LPLBlN4OpdtgwETAEe0thbg4F+jX4jIQxA1l5fFcg1UzgTJVPWtzTcoRs6bPlQYQTsy3wDSZySOh5XJWMkZzvC7zqrSgT+3GbbhiFveyyc4IJ/Up8zdIEocKYa3nKs821XnQuwkUIHlxh30G+MxJrY2GRpKo2/YRTBmIe9fA11a5sWFieoVju3xPg2Bbh/YMVzqai1KK8ckWwEOhUP7JrIF+mGW0uo0UM+Zo3EXoKHwzFN2hpVOSlmPdCnEiZvFTC+7PoEJEG8t0+CI2OEYkgrxcIOrfSrkjynwXqRMx5+KBVYyNwo/l/ULbLdKevlgby5gx8JIGIHQOmFCPI4wVipmlNNzZbFNXG9Zf+v8AXmgjeGbN7Px8AAAAASUVORK5CYII=" alt='logoLike'/> */}
-          {/* <div className="Like">{item.likes.length}</div>
-        </div>
-      </div> */}
       <div className="card">
         <div className="card-img">
           <img className='Itinerary-photo' src={item.user.photo} alt={item.user.name}></img>
