37d38659be08f69d5ed2f9322e63035d1e7f346d
adrieldussex <adrieldussex@gmail.com>
2022-09-22 04:04:02 -0300

    Modified Itinerary Component

--- src/components/ItineraryPage.js
+++ src/components/ItineraryPage.js
@@ -9,33 +9,25 @@ let user = JSON.parse(localStorage.getItem("user"))
 let userPhoto = user.photo
 
     return (       
-    <div className='Itinerary-container' key={item._id}>        
-         <div className='Itinerary-card'>
-             <div className='Itinerary-photo'> <img className='Itinerary-photo' src={userPhoto} alt={user.name}></img> </div>
-             <div className='Itinerary-description'>                
-                 <div className='Itinerary-name'>
-                     <h3>{item.name} </h3>
-                     <p>{item.user.name}</p>
-                 </div>
-                 <div className='Itinerary-info'>
-                     <p>Was a nice place</p>
-                     <div className='Info-price-duration'>
-                         <p>Price: {item.price}</p>
-                         <p>Duration: {item.duration}</p>
-                     </div>
-                     <div className='Info-tags'>
-                         <p> {item.name} </p>                        
-                     </div>
-                 </div>
-             </div>
-             <div>
-                 <img className='Like' src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAW9JREFUSEvVlUFWwjAQhr+h7OUIeALxBOIN5Alr5QR6FD0BurY+uIF4g3oCOALugfElaWoorQ0L9Jld8qf/P/NnZiocecmR+fkjgVSvgBugl2e4BB4ZyszuHX4HdHM8A54LPLBlN4OpdtgwETAEe0thbg4F+jX4jIQxA1l5fFcg1UzgTJVPWtzTcoRs6bPlQYQTsy3wDSZySOh5XJWMkZzvC7zqrSgT+3GbbhiFveyyc4IJ/Up8zdIEocKYa3nKs821XnQuwkUIHlxh30G+MxJrY2GRpKo2/YRTBmIe9fA11a5sWFieoVju3xPg2Bbh/YMVzqai1KK8ckWwEOhUP7JrIF+mGW0uo0UM+Zo3EXoKHwzFN2hpVOSlmPdCnEiZvFTC+7PoEJEG8t0+CI2OEYkgrxcIOrfSrkjynwXqRMx5+KBVYyNwo/l/ULbLdKevlgby5gx8JIGIHQOmFCPI4wVipmlNNzZbFNXG9Zf+v8AXmgjeGbN7Px8AAAAASUVORK5CYII=" alt='logoLike'/>
-                 <div className='Like'>{item.likes.length}</div>
-             </div>   
-         </div>
-        <EditItinerary/>             
-        <Activities/>   
-        <Comments/>       
-    </div>
+        <div className="Itinerary-container" key={item._id}>
+        <div className="card">
+          <div className="card-img">
+            <img className='Itinerary-photo' src={item.user.photo} alt={item.user.name}></img>
+          </div>
+          <div className="card-info">
+            <p className="text-title">{item.name}</p>
+            <p className="text-user">By {item.user.name}{''} {item.user.lastName}</p>
+            <div className="info-pd">
+              <p className="text-body">Price: {"💵 ".repeat(item.price)}</p>
+              <p className="text-body">Duration: {item.duration}hs</p>
+            </div>
+            <p className="text-tags">{item.tags.map(tag => "#" + tag + " ")}</p>
+          </div>
+        </div>
+        <EditItinerary/>
+        <Activities itineraryId={item._id}/>
+        <Comments itineraryId={item._id}/>
+        <hr className="horizontal-line"></hr>
+      </div>
   )
 }
