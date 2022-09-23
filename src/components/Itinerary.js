f619ac47d8b95dd6c0dbb8af0f0a744609d8934f
Richard Reyes <30903763+ReyesRRV@users.noreply.github.com>
2022-09-23 01:54:33 -0300

    Update Itinerary.js

--- src/components/Itinerary.js
+++ src/components/Itinerary.js
@@ -4,7 +4,10 @@ import Activities from "./Activities";
 import Comments from "./Comments";
 import Alert from "../components/Alert/Alert";
 import { useCreateActivityMutation } from "../features/activitiesAPI";
-import {useDelItineraryMutation} from "../features/itineraryAPI";
+import {
+  useDelItineraryMutation,
+  useEditItineraryMutation,
+} from "../features/itineraryAPI";
 
 export default function Itinerary({ item }) {
   let user = localStorage.getItem("user")
@@ -15,39 +18,41 @@ export default function Itinerary({ item }) {
   let [idItinerary, setIdItinerary] = useState();
   let nameRef = useRef();
   let photoRef = useRef();
+  let nameIRef = useRef();
+  const priceRef = useRef();
+  const durationRef = useRef();
+  const tagsRef = useRef();
 
   let [addActivityRedux, { data: activityAdded }] = useCreateActivityMutation();
-  let msg = "";
+  let [deleteItineraryRedux] = useDelItineraryMutation();
+  let [editItineraryRedux] = useEditItineraryMutation();
+let [msg , setMsg] = useState("")
+  
 
   let form = [
-    {
-      Name: "Name",
-      Label: "Name",
-      Placeholder: "Activity",
-      type: "text",
-      value: nameRef,
-    },
-    {
-      Name: "photo",
-      Label: "photo",
-      Placeholder: "Photo",
-      type: "text",
-      value: photoRef,
-    },
+    { Name: "Name",Label: "Name", Placeholder: "Activity",  type: "text", value: nameRef,},
+    {Name: "photo",Label: "photo", Placeholder: "Photo",type: "text",value: photoRef,},
+  ];
+  let formItinerary = [
+    {label: "Name", htmlFor: "Name", type: "text", placeholder: `${item.name}`,value: nameIRef,},
+    {label: "Price",htmlFor: "Price", type: "number",placeholder: `${item.price} $`,value: priceRef,},
+    {label: "Duration",htmlFor: "Duration",type: "number",placeholder: `${item.duration} hrs`,value: durationRef,},
+    {label: "Tags", htmlFor: "Tags",type: "String",placeholder: `${item.tags}`,value: tagsRef,},
   ];
 
-  function delItinerary() {
-    console.log("Del itinerary");
+  function delItinerary(e) {
+    deleteItineraryRedux(e.target.id);
   }
-  function editItinerary() {
-    console.log("Edit Itinerary");
+  function editButton() {
+    /// Button for show the "Add activity"
+    edit ? setedit(false) : setedit(true);
   }
-
   function addActivity() {
-    console.log("hola");
-    setOpen(true);
+    /// Button for show the "Add activity"
+    open ? setOpen(false) : setOpen(true);
   }
   function createActivity(e) {
+    ///// Create function for activities
     e.preventDefault();
     let dataActivity = {
       name: nameRef.current.value,
@@ -57,98 +62,113 @@ export default function Itinerary({ item }) {
     console.log(dataActivity);
     addActivityRedux(dataActivity);
   }
-  function cancelCreate() {
-    setOpen(false);
-  }
+  async function  editItinerary(e) {
+   e.preventDefault();
+   let id = nameIRef.current.id
 
-  return (
-    <div className="Itinerary-container">
-      <div className="card">
-        <div className="card-img">
-          <img
-            className="Itinerary-photo"
-            src={item.user.photo}
-            alt={item.user.name}
-          ></img>
+   const dataItinerary = {
+    name: nameIRef.current.value !== "" ? nameIRef.current.value : `${item.name}`,
+    price: priceRef.current.value !== "" ? priceRef.current.value : `${item.price}`,
+    duration: durationRef.current.value !== "" ? durationRef.current.value : `${item.duration}` ,
+    tags: tagsRef.current.value !== "" ? [tagsRef.current.value] : [`${item.tags}`]
+  };
+let res = await editItineraryRedux({dataItinerary, id })
+res.data  ? setMsg(res.data.message) : setMsg(res.error.data.message)
+
+}
+
+  return edit ? (
+    <form onSubmit={editItinerary}>
+      {formItinerary.map((e) => (
+        <div className="EditProfile-form" key={e.label}>
+          <label className="EditProfile-labels" htmlFor={item._d}>
+            {e.label}
+          </label>
+          <input
+            className="EditProfile-inputs"
+            id={item._id}
+            placeholder={e.placeholder}
+            ref={e.value}
+          ></input>
         </div>
-        <div className="card-info">
-          <p className="text-title">{item.name}</p>
-          <p className="text-user">
-            By {item.user.name}
-            {""} {item.user.lastName}
-          </p>
-          <div className="info-pd">
-            <p className="text-body">Price: {"💵 ".repeat(item.price)}</p>
-            <p className="text-body">Duration: {item.duration}hs</p>
-          </div>
-          <p className="text-tags">{item.tags.map((tag) => "#" + tag + " ")}</p>
-        </div>
+      ))}
+      <div className="EditProfile-buttons">
+        <Alert label={"Send"} message={msg} />
+        <button className="Profile-buttonEdit" onClick={editButton}>
+          {" "}
+          Cancel{" "}
+        </button>
       </div>
-      {user.id == item.user._id ? (
-        <>
-          <button onClick={addActivity}> Add Activity </button>
-          <button onClick={editItinerary}> Edit </button>
-          <button onClick={delItinerary}> Delete </button>
-        </>
-      ) : (
-        ""
-      )}
-      {open ? (
-        <form onSubmit={createActivity}>
-          {form.map((e) => (
-            <div className="EditProfile-form" key={e.Name}>
-              <label className="EditProfile-labels" htmlFor={item._id}>
-                {e.Name}
-              </label>
-              <input
-                className="EditProfile-inputs"
-                id={item._id}
-                placeholder={e.Placeholder}
-                ref={e.value}
-              ></input>
-            </div>
-          ))}
-          <div className="EditProfile-buttons">
-            <Alert label={"Send"} message="" />
-            <button className="Profile-buttonEdit" onClick={cancelCreate}>
-              {" "}
-              Cancel{" "}
-            </button>
+    </form>
+  ) : (
+    <>
+      <div className="Itinerary-container">
+        <div className="card">
+          <div className="card-img">
+            <img
+              className="Itinerary-photo"
+              src={item.user.photo}
+              alt={item.user.name}
+            ></img>
           </div>
-        </form>
-      ) : (
-        ""
-      )}
-      {edit ? (
-        <form onSubmit={editItinerary}>
-          {form.map((e) => (
-            <div className="EditProfile-form" key={e.Name}>
-              <label className="EditProfile-labels" htmlFor={e.Name}>
-                {e.Name}
-              </label>
-              <input
-                className="EditProfile-inputs"
-                id={e.Name}
-                placeholder={e.Placeholder}
-                ref={e.value}
-              ></input>
+          <div className="card-info">
+            <p className="text-title">{item.name}</p>
+            <p className="text-user">
+              By {item.user.name}
+              {""} {item.user.lastName}
+            </p>
+            <div className="info-pd">
+              <p className="text-body">Price: {"💵 ".repeat(item.price)}</p>
+              <p className="text-body">Duration: {item.duration}hs</p>
             </div>
-          ))}
-          <div className="EditProfile-buttons">
-            <Alert label={"Send"} message="" />
-            <button className="Profile-buttonEdit" onClick={cancelCreate}>
-              {" "}
-              Cancel{" "}
-            </button>
+            <p className="text-tags">
+              {item.tags.map((tag) => "#" + tag + " ")}
+            </p>
           </div>
-        </form>
-      ) : (
-        ""
-      )}
+        </div>
+        {user.id === item.user._id ? (
+          <>
+            <button onClick={addActivity}> Add Activity </button>
+            <button onClick={editButton}> Edit </button>
+            <button onClick={delItinerary} id={item._id}>
+              {" "}
+              Delete{" "}
+            </button>
+          </>
+        ) : (
+          ""
+        )}
+        {open ? (
+          <form onSubmit={createActivity}>
+            {form.map((e) => (
+              <div className="EditProfile-form" key={e.Name}>
+                <label className="EditProfile-labels" htmlFor={item._id}>
+                  {e.Name}
+                </label>
+                <input
+                  className="EditProfile-inputs"
+                  id={item._id}
+                  placeholder={e.Placeholder}
+                  ref={e.value}
+                ></input>
+              </div>
+            ))}
+            <div className="EditProfile-buttons">
+              <Alert label={"Send"} message="" />
+              <button className="Profile-buttonEdit" onClick={addActivity}>
+                {" "}
+                Cancel{" "}
+              </button>
+            </div>
+          </form>
+        ) : (
+          ""
+        )}
 
-      <Activities itineraryId={item._id} />
-      <Comments itineraryId={item._id} />
-      <hr className="horizontal-line"></hr>
-    </div>
+        <Activities itineraryId={item._id} />
+        <Comments itineraryId={item._id} />
+        <hr className="horizontal-line"></hr>
+      </div>
+    </>
   );
 }
